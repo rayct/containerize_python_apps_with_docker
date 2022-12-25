076d42c514e7792c3506a4d42bf15955e32fe809
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-12-25 18:31:46 +0000

    Created a FastAPI with a single endpoint using Docker

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Containerize Python Applications with Docker
+# 1. Containerize Python Applications with Docker
+
+# 2. Fast API, Single Endpoint using Docker
 
 ## Containerize Python applications using Docker is a key skill in the industry.
 
--- main.py
+++ main.py
@@ -1,21 +1,70 @@
 # Written By: Raymond Turner @rayturner.dev
+# Fast API with a Single endpoint using Docker
+import uvicorn
+from fastapi import FastAPI
 
 
-import time
-import socket
-from sklearn.datasets import load_iris
+app = FastAPI()
 
-data = load_iris()
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("0.0.0.0", 9999))
+@app.get("/")
+def central_function():
+    return{"Ray": "Colin"}
 
-server.listen()
 
-while True:
-    client, addr = server.accept()
-    print("Connection from" , addr)
-    client.send("You are Connected!\n".encode())
-    client.send(f"{data['data'][:, 0]}\n".encode())
-    time.sleep(2)
-    client.send("You are being disconnected!\n".encode())
-    client.close()
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import time
+# import socket
+# from sklearn.datasets import load_iris
+
+# data = load_iris()
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind(("0.0.0.0", 9999))
+
+# server.listen()
+
+# while True:
+#     client, addr = server.accept()
+#     print("Connection from" , addr)
+#     client.send("You are Connected!\n".encode())
+#     client.send(f"{data['data'][:, 0]}\n".encode())
+#     time.sleep(2)
+#     client.send("You are being disconnected!\n".encode())
+#     client.close()
